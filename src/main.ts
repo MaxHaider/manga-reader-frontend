5b9d7e423d6d9144146d4ab5ad6c20a1adcd4939
Max <max.haider@outlook.com>
2018-01-07 14:08:07 +0100

    :bug: delted some buggy lines

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,7 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-<<<<<<< HEAD
 import { AppModule } from './app/modules/app.module';
-=======
-import { AppModule } from './app/app.module';
->>>>>>> ab1e760... :sparkle: fresh cli setup
 import { environment } from './environments/environment';
 
 if (environment.production) {
